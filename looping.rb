c5b2c5bb1caf4b718afe3641a24cdb2dca16d1bc
zachlindop <85177168+zachlindop@users.noreply.github.com>
2021-08-29 22:58:48 -0700

    commit tree e1ecb26f67b460813e62bac4e37d9e5f30e2ac36

--- looping.rb
+++ looping.rb
@@ -1,6 +1,19 @@
 def happy_new_year
-  # your code here
+  i=10
+  while i>0
+    puts i 
+    i --1
+    puts "Happy New Year!"
 end
+
+#counter=10
+  #until counter==1
+   # puts "Happy New Year!"
+    #counter -= 1
+
+#Write a method #happy_new_year using a while or until loop 
+#that outputs numbers starting at 10 and counting down to 1. 
+#After reaching 1, print out "Happy New Year!"
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
 def fizzbuzz(num)
